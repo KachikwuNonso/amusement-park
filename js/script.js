c8a885a67a97338f0f5ec1568d22a1d5bc88b70a
Kachikwu Chukwunonso <kachikwuchukwunonso726@gmail.com>
2024-10-04 11:09:49 +0100

    add the img

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
 $(document).ready(function () {
     const height = parseInt(prompt("Please enter your height"))
 
-    if (height > 3 < 5){
+    if (height === 3, 4 ){
         $("#three").show();
-    } else if (height === 3) {
+        alert("Have Fun!")
+    } else if (height === 5) {
         alert("Have Fun!");
-        $("#three").show();
-    } else {
+        $("#five").show();
+    }else {
         $("#Under-3").show();
     }
 
-    if (height > 5){
-        $("#five").show();
-    } else if (height === 5) {
-        alert("Enjoy!");
-        $("#five").show();
-    } else {
-        $("#Under-5").show();
-    }
 
 })
